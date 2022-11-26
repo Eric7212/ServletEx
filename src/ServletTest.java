ba26e39307fad90b64a1f5224caf56388e27b30e
Eric <Ben541X@163.com>
2022-11-26 20:25:54 +0800

    实现将信息打印到浏览器上

--- src/ServletTest.java
+++ src/ServletTest.java
@@ -2,6 +2,7 @@ package com.eric.servlet;
 
 import javax.servlet.*;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class ServletTest implements Servlet{
 	//实现5个方法
@@ -11,7 +12,19 @@ public class ServletTest implements Servlet{
 	
 	public void service(ServletRequest req, ServletResponse resp) 
 	throws ServletException,IOException{
-		System.out.println("My Servlet");
+		//如何将一个信息直接输出到浏览器上面？
+		//需要使用ServletResponse接口
+		//Response表示响应：从服务器向浏览器发送数据叫做响应。
+		
+		
+		PrintWriter out = resp.getWriter();
+		
+		out.print("Print Message To Browser");
+		
+		//这是一个输出流，负责输出字符串到浏览器
+		//这个输出流不需要我们刷新，也不需要我们关闭，这些由Tomcat来维护
+		//out.flush();
+		//out.close();
 	} 
 	
 	public void destroy(){
